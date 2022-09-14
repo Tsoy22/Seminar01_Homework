a00df97f5775e08514d5d06751bb0a8a80459735
Vitaly Tsoy <tsoyvitalik@icloud.com>
2022-09-14 16:07:03 +0300

    task08

---
+++ task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4; 8 -> 2, 4, 6, 8;
+
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = number * (0) + 1;
+
+while (count <= number)
+{
+  if (count % 2 == 0)
+  {
+    Console.Write(count + " ; ");
+  }
+  count++;
+}
